add270c480e73c10d9c6fe9460c31f1572160d74
System Administrator <alexgoiadev@gmail.com>
2019-11-17 22:33:23 +0100

    Added refreshToken

--- server.js
+++ server.js
@@ -2,21 +2,17 @@ const
     cors = require('cors'),
     express = require('express'),
     compression = require('compression'),
-    path = require('path'),
     app = express(),
     bodyParser = require('body-parser'),
-    request = require('request');
-
-const PORT = process.env.PORT || 3000;
-const API_URL = "https://accounts.spotify.com/api/token";
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const CLIENT_CALLBACK_URL = process.env.CLIENT_CALLBACK_URL;
+    request = require('request'),
+    PORT = process.env.PORT || 3000,
+    API_URL = "https://accounts.spotify.com/api/token",
+    CLIENT_ID = process.env.CLIENT_ID,
+    CLIENT_SECRET = process.env.CLIENT_SECRET,
+    CLIENT_CALLBACK_URL = process.env.CLIENT_CALLBACK_URL;
 
 app.use(compression());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
     origin: true,
@@ -44,7 +40,18 @@ app.post('/exchange', (req, res) => {
 
 // Get a new access token from a refresh token
 app.post('/refresh', (req, res) => {
-
+    request.post(API_URL, {
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: req.body.refreshToken
+        }
+    }, (err, respuesta, body) => {
+        if (err) {
+            res.send({ err });
+        } else {
+            res.send(body);
+        }
+    });
 });
 
 
